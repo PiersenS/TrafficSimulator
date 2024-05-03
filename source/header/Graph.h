47dfa3eb659be5cad06d937acf06586c50d4822f
BuildTools <unconfigured@null.spigotmc.org>
2024-05-03 15:20:06 -0400

    changed spacing in Graph.h

--- source/header/Graph.h
+++ source/header/Graph.h
@@ -1,19 +1,26 @@
 #ifndef Graph_H
 #define Graph_H
 
+#include "Vertex.h"
+#include "Edge.h"
 #include <string>
 #include <vector>
 using namespace std;
 
 class Graph {
-    /*
-    * Reasons to use a header file for Graph:
-    *   - implementing multiple graphs
-    *   - abstraction for adjacency matrix, nodes, etc.
-    */
-    private:
-    public:
-    Graph() {}
+private:
+    vector<Vertex> vertices;
+    vector<Edge> edges;
+public:
+    Graph();
+    vector<Vertex> getVertices();
+    vector<Edge> getEdges();
+    
+    void insertVertex(Vertex v);
+    void insertEdge(Edge e, Vertex origin, Vertex dest);
+
+    void removeVertex(Vertex v);
+    void removeEdge(Edge e);
 };
 
 #endif
