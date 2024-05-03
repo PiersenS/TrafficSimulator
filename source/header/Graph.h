9dae46804dcedf485aa9c12f0f8d60e4387d925c
Piersen Schuiling <piersenschuiling@gmail.com>
2024-05-02 21:11:40 -0400

    Added planning notes in txt files and Graph.h

--- source/header/Graph.h
+++ source/header/Graph.h
@@ -1,10 +1,25 @@
 #ifndef Graph_H
 #define Graph_H
 
+#include <string>
+#include <vector>
+using namespace std;
 
+struct Node {
+    /* Each node is an intersection
+    *  store each node's neighbors
+    *       - use vector of id's
+    */
+    int id;
+    vector<int> adjacentNodes;
+};
 
 class Graph {
-
+    /*
+    * Reasons to use a header file for Graph:
+    *   - implementing multiple graphs
+    *   - abstraction for adjacency matrix, nodes, etc.
+    */
 };
 
 #endif
